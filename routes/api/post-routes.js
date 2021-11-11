7f75a8a138e0612f4e049e00d4b6fbb5897b1bc8
SNValadez <snvaladez13@gmail.com>
2021-11-10 19:46:12 -0800

    create post-routes

---
+++ routes/api/post-routes.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const { Post, User } = require('../../models');
+
+// get all users
+router.get('/', (req, res) => {
+    console.log('======================');
+    Post.findAll({
+        attributes: ['id', 'post_url', 'title', 'created_at'],
+    })
+  
+  });
