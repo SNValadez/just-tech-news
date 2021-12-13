68c3f87e7dff92c20edced96541c11d570f20518
S. N. Valadez <88351213+SNValadez@users.noreply.github.com>
2021-12-12 19:30:32 -0800

    update routes

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,27 +1,35 @@
 const router = require('express').Router();
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 const sequelize = require('../../config/connection');
 
 // get all users
 //* /api/posts
 router.get('/', (req, res) => {
-    Post.findAll({
-      attributes: ['id', 'post_url', 'title', 'created_at',
+  Post.findAll({
+    order: [['created_at', 'DESC']],
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
       [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-      ],
-      order: [["created_at", "DESC"]],
-      include: [
-        {
+    ],
+    include: [
+      // include the Comment model here:
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
           model: User,
           attributes: ['username']
         }
-      ]
-    })
-      .then(dbPostData => res.json(dbPostData))
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+   })
   });
 
 //*  /api/posts/:id
