3c2a31ad4aa7f46ac305bae9feb873298048d5cf
S. N. Valadez <88351213+SNValadez@users.noreply.github.com>
2022-03-01 21:24:42 -0800

    post space

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -58,6 +58,7 @@ router.get('/:id', (req, res) => {
     });
   });
 
+  
 // POST /api/users
 router.post('/', (req, res) => {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
